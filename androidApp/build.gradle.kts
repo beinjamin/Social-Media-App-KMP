b85e4da68e36b3d9b037138e50fed0ab11f163fa
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-01-31 13:03:53 +0100

    Mise a jour architecture

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" // Depends on your kotlin version
+    //Kotlinx Serialization
+    kotlin("plugin.serialization") version "1.9.10"
+
 }
 
 android {
@@ -45,4 +49,28 @@ dependencies {
     implementation(libs.compose.material3)
     implementation(libs.androidx.activity.compose)
     debugImplementation(libs.compose.ui.tooling)
+
+    implementation(project(":shared"))
+    implementation("androidx.compose.ui:ui:1.4.3")
+    implementation("androidx.compose.ui:ui-tooling:1.4.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    implementation("androidx.compose.foundation:foundation:1.4.3")
+    implementation("androidx.compose.material:material:1.4.3")
+    implementation("androidx.activity:activity-compose:1.7.2")
+
+    implementation("io.github.raamcosta.compose-destinations:core:1.8.38-beta")
+    ksp("io.github.raamcosta.compose-destinations:ksp:1.8.38-beta")
+
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    implementation("io.insert-koin:koin-androidx-compose:3.4.1")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
+
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
 }
