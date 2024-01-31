c1a04ec46efab48b96835bffbdec65726ad770ee
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-01-31 13:17:16 +0100

    Mise en place du model

--- androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpViewModel.kt
+++ androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpViewModel.kt
@@ -1,4 +1,14 @@
 package com.example.socialmediaapp.android.auth.signup
 
-class SignUpViewModel {
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel:ViewModel() {
 }
+
+data class  SignUpState(
+    var username: String = "",
+    var email: String = "",
+    var password: String = ""
+
+
+)
