a322005e28496ea9f5c8264f6af528363d7c1c31
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-01-31 13:27:32 +0100

    Mise a jour du state

--- androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpViewModel.kt
+++ androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpViewModel.kt
@@ -1,8 +1,26 @@
 package com.example.socialmediaapp.android.auth.signup
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class SignUpViewModel:ViewModel() {
+    var uiState by mutableStateOf(SignUpState())
+    private set
+
+
+    fun updateUsername(input: String){
+        uiState = uiState.copy(username=input)
+    }
+
+    fun updateEmail(input: String){
+        uiState = uiState.copy(email = input)
+    }
+
+    fun updatePassword(input: String){
+        uiState = uiState.copy(password = input)
+    }
 }
 
 data class  SignUpState(
