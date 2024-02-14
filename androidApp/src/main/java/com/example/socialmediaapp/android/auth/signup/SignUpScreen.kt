2ce5106f0a852cc087d27547ab833578e3aae290
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-02-14 15:31:22 +0100

    Mise en place interface des propriete de la colonne

--- androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpScreen.kt
+++ androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpScreen.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.socialmediaapp.android.common.theming.ExtraLargeSpacing
+import com.example.socialmediaapp.android.common.theming.LargeSpacing
+import com.example.socialmediaapp.android.common.theming.MediumSpacing
 
 @Composable
 fun SignUpScreen(
@@ -29,7 +33,15 @@ fun SignUpScreen(
             MaterialTheme.colorScheme.surface
         }
     )
-  ){
+         .padding(
+             top= ExtraLargeSpacing+ LargeSpacing,
+             start = LargeSpacing+ MediumSpacing,
+             end = LargeSpacing + MediumSpacing,
+             bottom = LargeSpacing
+         ),
+
+  )
+  {
 
   }
 }
