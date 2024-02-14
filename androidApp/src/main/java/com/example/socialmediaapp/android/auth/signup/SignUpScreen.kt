e37c22eab0f474ceb1dc1f4a06ce227e88dfd0df
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-02-14 15:28:27 +0100

    Mise en place interface des propriete de la colonne

--- androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpScreen.kt
+++ androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpScreen.kt
@@ -1,7 +1,12 @@
 package com.example.socialmediaapp.android.auth.signup
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -14,8 +19,16 @@ fun SignUpScreen(
     onPasswordChange:(String)->Unit,
 ){
   Column(
-modifier=modifier
+     modifier= modifier
     .fillMaxSize()
+    .verticalScroll(rememberScrollState())
+    .background(
+        color = if (isSystemInDarkTheme()){
+            MaterialTheme.colorScheme.background
+        }else{
+            MaterialTheme.colorScheme.surface
+        }
+    )
   ){
 
   }
