f74108c5d0c25a360ef9567e67e7e23640df7fcb
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-02-16 22:47:45 +0100

    Mise a jour CustomTextField

--- androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpScreen.kt
+++ androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpScreen.kt
@@ -2,6 +2,7 @@ package com.example.socialmediaapp.android.auth.signup
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,7 +10,10 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.socialmediaapp.android.R
+import com.example.socialmediaapp.android.common.components.CustomTextField
 import com.example.socialmediaapp.android.common.theming.ExtraLargeSpacing
 import com.example.socialmediaapp.android.common.theming.LargeSpacing
 import com.example.socialmediaapp.android.common.theming.MediumSpacing
@@ -24,24 +28,36 @@ fun SignUpScreen(
 ){
   Column(
      modifier= modifier
-    .fillMaxSize()
-    .verticalScroll(rememberScrollState())
-    .background(
-        color = if (isSystemInDarkTheme()){
-            MaterialTheme.colorScheme.background
-        }else{
-            MaterialTheme.colorScheme.surface
-        }
-    )
+         .fillMaxSize()
+         .verticalScroll(rememberScrollState())
+         .background(
+             color = if (isSystemInDarkTheme()) {
+                 MaterialTheme.colorScheme.background
+             } else {
+                 MaterialTheme.colorScheme.surface
+             }
+         )
          .padding(
-             top= ExtraLargeSpacing+ LargeSpacing,
-             start = LargeSpacing+ MediumSpacing,
+             top = ExtraLargeSpacing + LargeSpacing,
+             start = LargeSpacing + MediumSpacing,
              end = LargeSpacing + MediumSpacing,
              bottom = LargeSpacing
          ),
-
+      horizontalAlignment = Alignment.CenterHorizontally,
+      verticalArrangement = Arrangement.SpacedBy(LargeSpacing)
   )
   {
+      CustomTextField(
+          value = uiState.username,
+          onValueChange = onUsernameChange,
+          hint = R.string.username_hint
+
+      )
+      CustomTextField(
+          value = uiState.email,
+          onValueChange = onEmailChange,
+          hint = R.string.email_hint)
+
 
   }
 }
