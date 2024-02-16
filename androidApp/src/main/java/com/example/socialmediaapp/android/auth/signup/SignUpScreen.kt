5513613ac136d3394b1778cf07f6951d5b2eb592
Jaures Beinjamin <jauresbeinjamin@gmail.com>
2024-02-16 23:12:07 +0100

    Finalisation de la view Signup

--- androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpScreen.kt
+++ androidApp/src/main/java/com/example/socialmediaapp/android/auth/signup/SignUpScreen.kt
@@ -5,15 +5,24 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.example.socialmediaapp.android.R
 import com.example.socialmediaapp.android.common.components.CustomTextField
+import com.example.socialmediaapp.android.common.theming.ButtonHeight
 import com.example.socialmediaapp.android.common.theming.ExtraLargeSpacing
 import com.example.socialmediaapp.android.common.theming.LargeSpacing
 import com.example.socialmediaapp.android.common.theming.MediumSpacing
@@ -56,8 +65,32 @@ fun SignUpScreen(
       CustomTextField(
           value = uiState.email,
           onValueChange = onEmailChange,
-          hint = R.string.email_hint)
+          hint = R.string.email_hint,
+          keyboardType = KeyboardType.Email
+          )
+      CustomTextField(
+          value = uiState.password,
+          onValueChange =onPasswordChange,
+          hint = R.string.password_hint,
+          keyboardType = KeyboardType.Password,
+          isPasswordTextField = true
+          )
+      
+      Button(onClick = {
 
+      /*TODO*/
+      } ,
+          modifier= modifier
+              .fillMaxWidth()
+              .height(ButtonHeight),
+          elevation = ButtonDefaults.elevation(
+              defaultElevation = 0.dp
+          ),
+          shape = MaterialTheme.shapes.medium
+          )
+      {
+          Text(text= stringResource(id = R.string.signup_button_hint))
+      }
 
   }
 }
